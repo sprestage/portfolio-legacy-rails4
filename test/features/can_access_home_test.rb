b430f5cfb3a61583232cba29b8f4deb1c847194d
Susan Prestage <susan.prestage@gmail.com>
2013-11-13 11:28:58 -0800

    fix a test broken by front page changes

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "CanAccessHome" do
   scenario "the test is sound" do
     visit root_path
-    page.must_have_content "Welcome aboard"
     page.must_have_content "Susan Prestage"
     page.wont_have_content "Goodbye All!"
   end
