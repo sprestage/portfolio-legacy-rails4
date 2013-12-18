940745aab32040a4406383f02946bb98fb1cfbbe
Susan Prestage <susan.prestage@gmail.com>
2013-12-17 20:08:31 -0800

    improve policy tests

--- test/features/posts/post_show_index_test.rb
+++ test/features/posts/post_show_index_test.rb
@@ -40,7 +40,7 @@ feature "Show post index" do
     page.wont_have_content posts(:unPublishedPostByEditor).title
   end
 
-  scenario "editor can follow link for published post in the index" do
+  scenario "editor can follow link for someone else's published post in the index" do
     # Given a logged in editor
     sign_in_editor
     visit posts_path
@@ -52,19 +52,40 @@ feature "Show post index" do
     page.text.must_include "Author:"
   end
 
-  scenario "editor can follow link for unpublished post in the index" do
+  scenario "editor can follow link for someone else's unpublished post in the index" do
     # Given a logged in editor
     sign_in_editor
     visit posts_path
     page.text.must_include posts(:unPublishedPostByAuthor).title
-    # When I click on someone else's published post
+    # When I click on someone else's unpublished post
     page.find(:xpath, '//*[@id="post_543653019"]').click_on "Show"
     # Then I should see that post's show page
     page.text.must_include "Content:"
     page.text.must_include "Author:"
   end
 
-  scenario "editor can go to show page for published post" do
+  scenario "author can follow link for someone else's published post in the index" do
+    # Given a logged in author
+    sign_in_author
+    visit posts_path
+    page.text.must_include posts(:publishedPostByEditor).title
+    # When I click on someone else's published post
+    page.find(:xpath, '//*[@id="post_558138588"]').click_on "Show"
+    # Then I should see that post's show page
+    page.text.must_include "Content:"
+    page.text.must_include "Author:"
+  end
+
+  scenario "author cannot follow link for someone else's unpublished post in the index" do
+    # Given a logged in author
+    sign_in_author
+    # When I should visit the posts index page
+    visit posts_path
+    # Then I don't even see the Show link for someone else's unpublished post
+    page.wont_have_content posts(:unPublishedPostByEditor).title
+  end
+
+  scenario "editor can go to show page for someone else's published post" do
     # Given a logged in editor
     sign_in_editor
     # When I visit the show page for someone else's published post
@@ -74,11 +95,31 @@ feature "Show post index" do
     page.text.must_include "Author:"
   end
 
-  scenario "editor can go to show page for unpublished post" do
+  scenario "editor can go to show page for someone else's unpublished post" do
     # Given a logged in editor
     sign_in_editor
-    # When I visit the show page for someone else's published post
+    # When I visit the show page for someone else's unpublished post
     visit post_path(posts(:unPublishedPostByAuthor))
+    # Then I should see that post's show page
+    page.text.must_include "Content:"
+    page.text.must_include "Author:"
+  end
+
+  scenario "author can go to show page for someone else's published post" do
+    # Given a logged in author
+    sign_in_author
+    # When I visit the show page for someone else's published post
+    visit post_path(posts(:publishedPostByEditor))
+    # Then I should see that post's show page
+    page.text.must_include "Content:"
+    page.text.must_include "Author:"
+  end
+
+  scenario "author can go to show page for someone else's unpublished post" do
+    # Given a logged in author
+    sign_in_author
+    # When I visit the show page for someone else's unpublished post
+    visit post_path(posts(:unPublishedPostByEditor))
     # Then I should see that post's show page
     page.text.must_include "Content:"
     page.text.must_include "Author:"
